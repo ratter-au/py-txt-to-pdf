1f106681451ff297755a91382839b68a0ac79e78
Ratter <149956067+ratter-au@users.noreply.github.com>
2026-05-21 18:31:55 +1000

    Added more detail to the pseudocode outline

--- txt-to-pdf.py
+++ txt-to-pdf.py
@@ -47,10 +47,21 @@ text_wrapper: Final[TextWrapper] = TextWrapper(
 
 # Write the PDF header and whatever prologue is necessary.  Set the page dimensions and font face & size.
 # Loop for each page:
+#     Let the text to be written to the current page be an empty string.
 #     If there was any overflow from the last paragraph on the last page, add it to the text to be written to the current page.
-#     Read lines from standard input until we either hit an empty line, indicating the end of a paragraph, or until we've filled a page.
-#     Wrap the paragraph and add its contents to the text to be written to the current page.
-#     If there is no text to be written to the current page (no overflow from the last page and no input), there's no need to write a new page; break out of the loop.
-#     If the number of lines in the current page would exceed the length of a page, truncate it, and save the remainder to be added to the start of the next page.
+#     Loop as long as there's input remaining and the text to be written to the current page does not exceed the number of lines on a page:
+#         Let the current paragraph be an empty string.
+#         Loop unconditionally:
+#             Attempt to read a line from standard input.
+#             If we hit end-of-file, record the fact that there's no input remaining, and break out of the loop.
+#             If we read an empty line (indicating the end of a paragraph), break out of the loop.
+#             Add the line to the current paragraph and continue looping over input lines.
+#         If the current paragraph is not empty:
+#             Wrap the current paragraph.
+#             Add the current paragraph to the text to be written to the current page.
+#         Continue looping over input paragraphs.
+#     If there is no text to be written to the current page (there was no overflow from the last page and no new paragraphs), there's no need to write a new page; break out of the loop.
+#     If the number of lines in the current page exceeds the length of a page, truncate it, and save the overflow to be added to the start of the next page.
 #     Create a new page in the PDF file and write all the text to it.
+#     Continue looping over pages.
 # After all pages have been written, write the PDF cross-reference table and footer.
