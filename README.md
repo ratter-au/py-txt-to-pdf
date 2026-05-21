4d033ba567c3f8885d152f41c9c5a20899fae5e4
Ratter <149956067+ratter-au@users.noreply.github.com>
2026-05-21 18:20:29 +1000

    Initial commit with rough sketch of what needs to be done

---
+++ README.md
@@ -0,0 +1,18 @@
+# `ratter-au/py-txt-to-pdf`
+
+Convert a plain text file into a bare‐bones PDF, such that the text extracted from the PDF matches the original as closely as possible.
+
+**This is a work in progress.**
+
+## Usage
+
+The script reads from standard input and writes to standard output.  Use the shell to redirect those streams appropriately:
+
+```
+python3 txt-to-pdf.py < example.txt > example.pdf
+```
+
+## License
+
+The contents of this repository are released under
+[the Creative Commons Attribution 4.0 International (CC-BY-4.0) license](./LICENSE.txt).
---
+++ txt-to-pdf.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""Convert a plain text file into a bare‐bones PDF, such that the text extracted from the PDF matches the original as closely as possible.
+
+The script reads from standard input and writes to standard output.  Use the shell to redirect those streams appropriately:
+
+```
+python3 txt-to-pdf.py < example.txt > example.pdf
+```
+"""
+
+from typing import (
+    Final,
+)
+from sys import (
+    stdin,
+    stdout,
+)
+from textwrap import (
+    TextWrapper,
+)
+
+# Hard-coded A4 paper size & portrait orientation for now
+PAGE_WIDTH_MM: Final[float] = 210.0
+PAGE_HEIGHT_MM: Final[float] = 297.0
+
+# Millimetres per typographic point
+FONT_POINT_MM: Final[float] = 0.3528
+
+# Use the built-in Courier monospace font
+FONT_FACE: Final[str] = "Courier"
+FONT_HEIGHT_PT: Final[int] = 12
+FONT_WIDTH_PT: Final[int] = 8
+
+# TODO: Compute these from the font metrics, paper size, and margins
+PAGE_COLUMNS: Final[int] = 74
+PAGE_LINES: Final[int] = 70
+
+text_wrapper: Final[TextWrapper] = TextWrapper(
+    width=PAGE_COLUMNS,
+    expand_tabs=False,
+    replace_whitespace=False,
+    break_long_words=False,
+    break_on_hyphens=False,
+)
+
+# Here's a tentative outline of what the main program should do.
+
+# Write the PDF header and whatever prologue is necessary.  Set the page dimensions and font face & size.
+# Loop for each page:
+#     If there was any overflow from the last paragraph on the last page, add it to the text to be written to the current page.
+#     Read lines from standard input until we either hit an empty line, indicating the end of a paragraph, or until we've filled a page.
+#     Wrap the paragraph and add its contents to the text to be written to the current page.
+#     If there is no text to be written to the current page (no overflow from the last page and no input), there's no need to write a new page; break out of the loop.
+#     If the number of lines in the current page would exceed the length of a page, truncate it, and save the remainder to be added to the start of the next page.
+#     Create a new page in the PDF file and write all the text to it.
+# After all pages have been written, write the PDF cross-reference table and footer.
